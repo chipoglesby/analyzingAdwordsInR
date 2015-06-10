45b7780673a7d7f87ba1d14b6c816331c8de3543
Chip Oglesby <chip@creativesparkcolumbia.com>
2015-06-10 10:07:35 -0400

    Create placement_performance_report.R

---
+++ placement_performance_report.R
@@ -0,0 +1,55 @@
+#Loop through all of these accounts and get the information you need
+
+library(RAdwords)
+library(dplyr)
+
+#Read in a list of Clients
+clients <- read.csv("clients.csv", header = TRUE, sep = ",")
+
+#Client ID's to account for looping
+accounts <- clients$Customer.ID
+
+# Authentication, once done you can access all accounts in your MCC
+google_auth <- doAuth()
+
+# Specify date range, i.e. last two weeks until yesterday
+yesterday <- gsub("-","",format(Sys.Date()-1,"%Y-%m-%d"))
+thirtydays<- gsub("-","",format(Sys.Date()-29,"%Y-%m-%d"))
+
+# Create statement
+body <- statement(select=c("AccountDescriptiveName", "CampaignName", "AdGroupName", "DisplayName","PlacementUrl","Impressions","Clicks","Ctr","AverageCpm","AverageCpc","Cost", "ConvertedClicks", "ViewThroughConversions"),
+                  report="PLACEMENT_PERFORMANCE_REPORT",
+                  where="AdNetworkType1 = CONTENT",
+                  start=thirtydays,
+                  end=yesterday)
+
+#Loop over accounts and rbind data to dataframe
+loopData <- function(){
+  for(i in 1:length(accounts)){
+    if (i == 1){
+      xy <- getData(clientCustomerId=accounts[i],statement=body,google_auth = google_auth,transformation=TRUE,changeNames=TRUE)
+    }
+    else {
+      xz <- getData(clientCustomerId=accounts[i],statement=body,google_auth = google_auth,transformation=TRUE,changeNames=TRUE)
+      xy <- rbind(xy,xz)
+    }
+  }
+  return(xy)
+}
+data <- loopData()
+
+placements <- data %>% group_by(CriteriaDisplayName) %>% summarize(
+  AverageCpc=round(sum(Cost) / sum(Clicks),2), 
+  Clicks=sum(Clicks), 
+  Impressions=sum(Impressions), 
+  CTR=round(sum(Clicks) / sum(Impressions),2), 
+  Cost=round(sum(Cost),2), 
+  Conversions=sum(Conversions), 
+  CPA=round(sum(Cost) / sum(Conversions),2))
+placements <- placements[order( -placements$Conversions),]
+placements[placements==Inf] <- 0
+View(placements)
+#write.csv(placements, "display_placements.csv")
+under_performing_placements <- subset(placements, Conversions < 1)
+under_performing_placements <- under_performing_placements[order(-under_performing_placements$Cost),]
+View(under_performing_placements)
